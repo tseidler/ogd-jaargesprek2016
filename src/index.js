0815e261fbbefe240c09bd74b840b2143c2926e9
Terry Seidler <terry.seidler@ogd.nl>
2016-12-17 19:10:08 +0100

    Refactor text renderer, add modules to gitignore

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import TextRenderer from 'jaargesprek2016/textrenderer';
 
-let textRenderer = new TextRenderer(document.querySelector('#textCanvas'));
-textRenderer.setText('Vraag 1: caek caek caek', 'Antwoord antwoord antwoord antwoord op vraag vraag vraag, blabla, antwoord bla antwoord', 100);
+let textRenderer = new TextRenderer(
+  document.querySelector('#textCanvas'),
+  'Vraag 1: caek caek caek',
+  'Antwoord antwoord antwoord antwoord op vraag vraag vraag, blabla, antwoord bla antwoord'
+);
--- src/textrenderer.js
+++ src/textrenderer.js
@@ -1,33 +1,95 @@
+const DEFAULT_QUESTION_FONT_SIZE = 30;    // pt
+const DEFAULT_ANSWER_FONT_SIZE = 20;      // pt
+const SPACE_BETWEEN_QUESTION_ANSWER = 50; // px
+
 export default class TextRenderer {
-  constructor(canvasElement) {
+  constructor(canvasElement, questionText, answerText) {
     this.canvas = canvasElement;
     this.context = this.canvas.getContext('2d');
 
+    this.questionTextWidth = 0;
+    this.answerFontSize = DEFAULT_ANSWER_FONT_SIZE;
+    this.question = questionText;
+    this.answer = answerText;
+
     window.addEventListener('resize', this.resizeCanvas.bind(this), false);
-
     this.resizeCanvas();
-    this.clearCanvas();
 
+    // disable image smoothing
     this.context['imageSmoothingEnabled'] = false;       /* standard */
     this.context['mozImageSmoothingEnabled'] = false;    /* Firefox */
     this.context['oImageSmoothingEnabled'] = false;      /* Opera */
-    this.context['webkitImageSmoothingEnabled'] = false; /* Safari */
     this.context['msImageSmoothingEnabled'] = false;     /* IE */
   }
 
-  setText(questionText, answerText, speed) {
-    this.clearCanvas();
-
-    this.question = questionText;
-    this.answer = '';
-
-    this.drawQuestion();
+  setText(question, answer) {
+    this.question = question;
+    this.answer = answer;
   }
 
-  drawQuestion() {
-    this.context.font = '40pt determinationSans';
-    this.context.fillStyle = '#FFFFFF';
-    this.context.fillText(this.question, this.canvas.width/3, this.canvas.height/4);
+  drawText() {
+    this.clearCanvas();
+    this.questionTextWidth = this.renderQuestion();
+    this.renderAnswer();
+  }
+
+  renderQuestion() {
+    let font = 'determinationSans';
+    let question_width = this.determineAndSetQuestionSize(this.question, font, this.canvas.width * .8);
+
+    let x = Math.max(50, (this.canvas.width - question_width)/2);
+    let y = this.yPos();
+
+    this.context.fillStyle = '#FFF';
+    this.context.fillText(this.question, x, y);
+
+    return question_width;
+  }
+
+  determineAndSetQuestionSize(text, font, max_width) {
+    let current_size = DEFAULT_QUESTION_FONT_SIZE;
+    let current_width = max_width;
+
+    this.context.font = `${current_size}pt ${font}`;
+    while((current_width = this.context.measureText(text).width) > max_width && current_size > 20) {
+      current_size -= 2;
+      this.context.font = `${current_size}pt ${font}`;
+    }
+    this.answerFontSize = Math.min(DEFAULT_ANSWER_FONT_SIZE, current_size - 5);
+    return current_width;
+  }
+
+  renderAnswer() {
+    this.context.font = `${this.answerFontSize}pt determinationMono`;
+    this.context.fillStyle = '#FFF';
+
+    let max_width = this.canvas.width * .8;
+    let x = (this.canvas.width - max_width) / 2;
+    let measuredWidth = this.context.measureText(this.answer).width;
+
+    if(measuredWidth < max_width || measuredWidth < this.questionTextWidth) {
+      x = this.canvas.width/2 - measuredWidth/2;
+    }
+
+    let line_height = this.answerFontSize * 2;
+    let y = this.yPos() + SPACE_BETWEEN_QUESTION_ANSWER;
+    let words = this.answer.split(' ');
+    let line = '';
+
+    for(var n = 0; n < words.length; n++) {
+      let test_line = line + words[n] + ' ';
+      let metrics = this.context.measureText(test_line);
+
+      if(metrics.width > max_width && n > 0) {
+        this.context.fillText(line, x, y);
+        line = words[n] + ' ';
+        y += line_height;
+      }
+      else {
+        line = test_line;
+      }
+    }
+    this.context.fillText(line, x, y);
   }
 
   clearCanvas() {
@@ -41,6 +103,10 @@ export default class TextRenderer {
     this.canvas.width = window.innerWidth;
     this.canvas.height = window.innerHeight;
 
-    this.drawQuestion();
+    this.drawText();
+  }
+
+  yPos() {
+    return this.canvas.height/4;
   }
 }
