f75b171b7cae3630d1ac75883e337283af29a089
Terry Seidler <terry.seidler@ogd.nl>
2016-12-17 23:56:50 +0100

    Add questions/answers source file

---
+++ src/questions_answers.js
@@ -0,0 +1,58 @@
+export const data = [
+  {
+    question: 'Hoe bevalt het bij OGD? Wat vind je goed gaan? Wat kan beter?',
+    answer: 'The first time I stole so that I wouldn\'t starve, yes. I lost many assumptions about the simple nature of right and wrong. And when I traveled I learned the fear before a crime and the thrill of success. But I never became one of them.',
+  },
+  {
+    question: 'Wat zijn je huidige taken/verantwoordelijkheden? Hoe bevalt dit?',
+    answer: 'But we\'ve met before. That was a long time ago, I was a kid at St. Swithin\'s, It used to be funded by the Wayne Foundation. It\'s an orphanage. My mum died when I was small, it was a car accident. I don\'t remember it. My dad got shot a couple of years later for a gambling debt. Oh I remember that one just fine. Not a lot of people know what it feels like to be angry in your bones. I mean they understand. The fosters parents. Everybody understands, for a while. Then they want the angry little kid to do something he knows he can\'t do, move on. After a while they stop understanding. They send the angry kid to a boy\'s home, I figured it out too late. Yeah I learned to hide the anger, and practice smiling in the mirror. It\'s like putting on a mask. So you showed up this one day, in a cool car, pretty girl on your arm. We were so excited! Bruce Wayne, a billionaire orphan? We used to make up stories about you man, legends and you know with the other kids, that\'s all it was, just stories, but right when I saw you, I knew who you really were. I\'d seen that look on your face before. It\'s the same one I taught myself. I don\'t why you took the fault for Dent\'s murder but I\'m still a believer in the Batman. Even if you\'re not...',
+  },
+  {
+    question: 'Beschrijf wat je qua kennis hebt ontwikkeld het afgelopen halfjaar.',
+    answer: 'placeholder',
+  },
+  {
+    question: 'Beschrijf hoe je in ervaring bent gegroeid het afgelopen halfjaar.',
+    answer: 'placeholder',
+  },
+  {
+    question: 'Beschrijf hoe je persoonlijk bent gegroeid in het afgelopen halfjaar.',
+    answer: 'placeholder',
+  },
+  {
+    question: 'Beschrijf iets uit het afgelopen halfjaar waar je trots op bent.',
+    answer: 'placeholder',
+  },
+  {
+    question: 'Welke belemmeringen ervaar in je in je werk? En hoe kan OGD jou daarmee helpen?',
+    answer: 'placeholder',
+  },
+  {
+    question: 'Waar liggen jouw ambities, wat vind je leuk, waar krijg je energie van?',
+    answer: 'placeholder',
+  },
+  {
+    question: 'Stel: Het is 2 jaar verder. Waar kijk je op terug?',
+    answer: 'placeholder',
+  },
+  {
+    question: 'Wat wil je gaan doen op het gebied van examens en cursussen?',
+    answer: 'placeholder',
+  },
+  {
+    question: 'Welke ervaring wil je het komende halfjaar opdoen?',
+    answer: 'placeholder',
+  },
+  {
+    question: 'Hoe wil je je persoonlijk gaan ontwikkelen?',
+    answer: 'placeholder',
+  },
+  {
+    question: 'Hoe ga je die ontwikkelingen verwezenlijken? Kunnen we je hierin ondersteunen?',
+    answer: 'placeholder',
+  },
+  {
+    question: 'Heb je verder nog vragen, opmerkingen of suggesties, vermeld deze dan hier.',
+    answer: 'placeholder',
+  }
+];
