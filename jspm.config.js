8568ec49af4a0e4f7a869b992f0f01e3e6760984
Terry Seidler <terry.seidler@ogd.nl>
2016-12-17 15:56:07 +0100

    Initial setup

---
+++ jspm.config.js
@@ -0,0 +1,54 @@
+SystemJS.config({
+  paths: {
+    "github:": "jspm_packages/github/",
+    "npm:": "jspm_packages/npm/",
+    "jaargesprek2016/": "src/"
+  },
+  browserConfig: {
+    "baseURL": "/"
+  },
+  devConfig: {
+    "map": {
+      "babel-runtime": "npm:babel-runtime@5.8.38",
+      "core-js": "npm:core-js@1.2.7",
+      "process": "npm:jspm-nodelibs-process@0.2.0",
+      "path": "npm:jspm-nodelibs-path@0.2.1",
+      "fs": "npm:jspm-nodelibs-fs@0.2.0",
+      "plugin-babel": "npm:systemjs-plugin-babel@0.0.17"
+    },
+    "packages": {
+      "npm:babel-runtime@5.8.38": {
+        "map": {}
+      },
+      "npm:core-js@1.2.7": {
+        "map": {
+          "systemjs-json": "github:systemjs/plugin-json@0.1.2"
+        }
+      }
+    }
+  },
+  transpiler: "plugin-babel",
+  babelOptions: {
+    "optional": [
+      "runtime",
+      "optimisation.modules.system"
+    ]
+  },
+  map: {
+    "babel": "npm:babel-core@5.8.38"
+  },
+  packages: {
+    "jaargesprek2016": {
+      "main": "jaargesprek2016.js"
+    }
+  }
+});
+
+SystemJS.config({
+  packageConfigPaths: [
+    "npm:@*/*.json",
+    "npm:*.json"
+  ],
+  map: {},
+  packages: {}
+});
