84cb6d68cb1a49b1bcbedaa3f7f2500811f5829c
Terry Seidler <terry.seidler@ogd.nl>
2016-12-19 21:16:50 +0100

    Pass images to navigator

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import Navigator from 'jaargesprek2016/navigator';
-import {data} from 'jaargesprek2016/questions_answers';
+import ImageCache from 'utils/imagecache';
+import {data, images} from 'jaargesprek2016/questions_answers';
 
-let navigator = new Navigator({
-  'data':   data,
-  'layers': {
-    'text':       document.querySelector('#layer_text'),
-    'background1': document.querySelector('#layer_background1'),
-    'background2': document.querySelector('#layer_background2')
-  }
+// Load all images, then start
+let imageCache = new ImageCache();
+imageCache.load(images, () => {
+  let navigator = new Navigator({
+    'data':   data,
+    'images': images,
+    'layers': {
+      'text':       document.querySelector('#layer_text'),
+      'background1': document.querySelector('#layer_background1'),
+      'background2': document.querySelector('#layer_background2')
+    }
+  });
 });
