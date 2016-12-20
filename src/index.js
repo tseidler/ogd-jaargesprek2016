850874b01d2c89ea06185ada2b9612bf2fa52baa
Terry Seidler <terry.seidler@ogd.nl>
2016-12-20 08:38:17 +0100

    Add music

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import Navigator from 'jaargesprek2016/navigator';
 import ImageCache from 'utils/imagecache';
 import {data, images} from 'jaargesprek2016/questions_answers';
 
+setTimeout(() => {
+  let audio = document.getElementById('music');
+  audio.volume = 0.05;
+  audio.loop = true;
+  audio.play();
+}, 5000);
+
 // Load all images, then start
 let imageCache = new ImageCache();
 imageCache.load(images, () => {
